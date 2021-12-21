78f02b6356c2017acfd9aa7e91dbe764baa0da32
VEANGLY <veang.kroh@student.passerellesnumeriques.org>
2021-12-21 10:41:49 +0700

    add Frame

---
+++ spacship-shooter.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+root = tk.Tk()
+root.geometry("1440x1000")
+frame = tk.Frame()
+canvas = tk.Canvas(frame)
+frame.pack(expan=True,fill="both")
+frame.master.title("The spacship Allien shooter!")
+canvas.pack(expan=True,fill="both")
+root.mainloop()
